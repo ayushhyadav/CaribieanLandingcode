7e6e771da1a74d0bb9ce14ee70d59d677debef57
ayushhyadav <ayushyadav8964@gmail.com>
2024-07-02 21:45:42 +0530

    users

--- Careb-V01/src/screens/Server/BaseUrl.js
+++ Careb-V01/src/screens/Server/BaseUrl.js
@@ -1,6 +1,6 @@
-// const BaseUrl = "https://sher9522.caribbeaneaze.com"
-const
- BaseUrl="http://localhost:5000"
+const BaseUrl = "https://sher9522.caribbeaneaze.com"
+// const
+//  BaseUrl="http://localhost:5000"
 export default {
     BaseUrl
 }
